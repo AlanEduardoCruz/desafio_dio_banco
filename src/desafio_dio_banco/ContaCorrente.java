2f7f92bdfb681830576976d2cda85e305063414c
Alan <alan-hd@hotmail.com>
2024-05-28 23:24:06 -0300

    Criando projeto

---
+++ src/desafio_dio_banco/ContaCorrente.java
@@ -0,0 +1,7 @@
+package desafio_dio_banco;
+
+//classe contaCorrente herda tudo da classe conta
+
+public class ContaCorrente extends Conta {
+
+}
---
+++ src/desafio_dio_banco/ContaPoupanca.java
@@ -0,0 +1,6 @@
+package desafio_dio_banco;
+
+// classe contaPoupança herda tudo da classe conta
+public class ContaPoupanca extends Conta {
+
+}
